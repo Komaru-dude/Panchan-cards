d0b7b0b710194dcccb6a1a389077a156aa61117e
Komaru-dude <honkhonkovic842@gmail.com>
2024-12-29 20:52:33 +0700

    Блокировка и разблокировка пользователей

--- bot/handlers/administration.py
+++ bot/handlers/administration.py
@@ -18,3 +18,33 @@ async def cmd_rc(message: types.Message):
         await message.reply(f"Кулдаун получения карточки сброшен для пользователя с ID: {target_user_id}")
     except:
         await message.reply(f"Не удалось сбросить кулдаун для пользователя с ID: {target_user_id}")
+
+@adm_router.message(Command("ban"))
+async def cmd_ban(message: types.Message):
+    user_id = message.from_user.id
+    if not db.has_permission(user_id, 3):
+        await message.reply("Вы не имеете прав для выполнения этой команды.")
+        return
+    
+    target_user_id = message.reply_to_message.from_user.id if message.reply_to_message else message.from_user.id
+    
+    try:
+        db.set_data(target_user_id, "rank", "Забанен")
+        await message.reply(f"Пользователь (ID: {target_user_id}) был заблокирован")
+    except Exception as e:
+        await message.reply(f"Не удалось заблокировать пользователя(ID: {target_user_id}).\n Ошибка: {e}")
+
+@adm_router.message(Command("unban"))
+async def cmd_unban(message: types.Message):
+    user_id = message.from_user.id
+    if not db.has_permission(user_id, 3):
+        await message.reply("Вы не имеете прав для выполнения этой команды.")
+        return
+    
+    target_user_id = message.reply_to_message.from_user.id if message.reply_to_message else message.from_user.id
+    
+    try:
+        db.set_data(target_user_id, "rank", "Участник")
+        await message.reply(f"Пользователь (ID: {target_user_id}) был разблокирован")
+    except Exception as e:
+        await message.reply(f"Не удалось разблокировать пользователя(ID: {target_user_id}).\n Ошибка: {e}")
